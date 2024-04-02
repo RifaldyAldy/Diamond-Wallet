217fee7db68bef8e82245de81b3fc524ca1c4a51
Nazlah <afnazlah89@gmail.com>
2024-04-02 09:06:11 +0700

    [FEATURE] info

--- delivery/controller/admin_controller.go
+++ delivery/controller/admin_controller.go
@@ -60,7 +60,6 @@ func (a *AdminController) Route() {
 	{
 		rg.POST("/", a.RegisterHandler)
 		rg.POST("/login", a.LoginHandler)
-		rg.GET("/user/:id", a.GetUserInfo)
 		rg.GET("/user/:id", common.JWTAuth("admin"), a.GetUserInfo)
 	}
 }
